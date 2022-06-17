528ca78413b60a7fbe89d299e457d223a10a8e80
Jonathan Finger <jaefinger@gmail.com>
2022-06-16 20:41:44 -0500

    Added sqllite dev db

--- sniffr/app.py
+++ sniffr/app.py
@@ -2,42 +2,55 @@ from flask import Flask, jsonify
 from dotenv import load_dotenv
 import os
 
-# from sniffr.models import db, migrate, Dog
-
 load_dotenv()
+import sys
+
+from sniffr.models import db, migrate, Dog
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 def create_app():
 
     app = Flask(__name__)
 
-    # # PostGres Connection
-    # app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("PG_DATABASE_URL")
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    if os.getenv("FLASK_ENV") == "development":
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+            basedir, "database.db"
+        )
 
-    # # Show flask where the tables are
-    # db.init_app(app)
-    # migrate.init_app(app, db)
+    elif os.getenv("FLASK_ENV") == "development":
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("PG_DATABASE_URL")
+
+    else:
+        print("Error: No environment specified")
+        sys.exit(1)
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Show flask where the tables are
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     @app.route("/")
     def our_first_route():
         return "<h3>Welcome to Sniffr's Backend! Feel free to take a whiff!</h3>"
 
-    # @app.route("/all_dogs")
-    # def all_dogs():
-    #     dogs = Dog.query.all()
-    #     return f"{dogs}"
+    @app.route("/all_dogs")
+    def all_dogs():
+        dogs = Dog.query.all()
+        return f"There are {len(dogs)} dogs in the database right now!"
 
-    # @app.route("/add_dog/<dog_name>/<dog_age>")
-    # def add_dog(dog_name, dog_age):
-    #     dog = Dog(dog_name=dog_name, dog_age=dog_age)
-    #     db.session.add(dog)
-    #     db.session.commit()
+    @app.route("/add_dog/<dog_name>/<dog_age>")
+    def add_dog(dog_name, dog_age):
+        dog = Dog(dog_name=dog_name, dog_age=dog_age)
+        db.session.add(dog)
+        db.session.commit()
 
-    #     # Return the dog that was just added
-    #     dog_id = db.session.query(Dog).get(dog.dog_id)
+        # Return the dog that was just added
+        dog_id = db.session.query(Dog).get(dog.dog_id)
 
-    #     return jsonify({"DogID": f"{dog_id}"})
+        return jsonify({"DogID": f"{dog_id}"})
 
     return app
 
