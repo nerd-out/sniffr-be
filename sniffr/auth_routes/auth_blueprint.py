45e66c5dc4a7607c9b4b9c1cad952c0c8a9dda4b
Jonathan Finger <jaefinger@gmail.com>
2022-07-09 17:34:48 -0500

    Added logout route and spruced up docstrings

--- sniffr/auth_routes/auth_blueprint.py
+++ sniffr/auth_routes/auth_blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from flask_cors import cross_origin
 from sniffr.models import User, db
 
@@ -9,6 +9,7 @@ auth_bp = Blueprint("auth_bp", __name__)
 @auth_bp.route('/login', methods=['POST'])
 @cross_origin()
 def login():
+    """ When a correct email and password is given, provide a success prompt"""
     content = request.json
     email = content['email']
     passwd = content['password']
@@ -16,12 +17,20 @@ def login():
     # Make sure 
     if email and passwd:
         exists = db.session.query(User.user_id).filter_by(email=f'{email}', password=f'{passwd}').first() is not None
-        
+
         if exists:
-            return jsonify({'status': 'success!'})
+            return {'message': 'success!'}
         
         else:
-            return jsonify({'status': 'fail'})
+            return {'message': 'fail'}, 400
 
     else:
-        return jsonify({'status': 'fail'})
+        return {'message': 'fail'}, 400
+
+
+@auth_bp.route('/logout', methods=['POST'])
+@cross_origin()
+def logout():
+    ''' Simulates a logout point. Doesn't do much until json webtokens are added'''
+
+    return {'message': 'success!'}
