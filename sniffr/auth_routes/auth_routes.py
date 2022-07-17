c34e0e3fe573ffe11a91f2dbbf5c40e2ac58b941
Jonathan Finger <jaefinger@gmail.com>
2022-07-16 20:49:03 -0500

    Added documentation

--- sniffr/auth_routes/auth_routes.py
+++ sniffr/auth_routes/auth_routes.py
@@ -5,55 +5,58 @@ from sniffr.models import User, db, process_records
 # Blueprint Configuration
 auth_bp = Blueprint("auth_bp", __name__)
 
-
-@auth_bp.route('/login', methods=['POST'])
+# Login route
+@auth_bp.route("/login", methods=["POST"])
 @cross_origin()
 def login():
-    """ When a correct email and password is given, provide a success prompt"""
+    """When a correct email and password is given, provide a success prompt"""
     content = request.json
-    email = content['email']
-    passwd = content['password']
+    email = content["email"]
+    passwd = content["password"]
 
-    # Make sure 
+    # Make sure email and password are provided
     if email and passwd:
-        # exists = db.session.query(User.user_id).filter_by(email=f'{email}').first()
-        result = db.session.query(User).filter_by(email=f'{email}').first()
+        result = db.session.query(User).filter_by(email=f"{email}").first()
 
+        # Check that there is a valid result and a correct password
         if result and result.verify_password(password=passwd):
-            return {'message': 'success!'}
-        
+            return {"message": "success!"}
+
         else:
-            return {'message': 'fail'}, 400
+            return {"message": "fail"}, 400
 
     else:
-        return {'message': 'fail'}, 400
+        return {"message": "fail"}, 400
 
 
-@auth_bp.route('/logout', methods=['POST'])
+# Logout route
+@auth_bp.route("/logout", methods=["POST"])
 @cross_origin()
 def logout():
-    ''' Simulates a logout point. Doesn't do much until json webtokens are added'''
+    """Simulates a logout point. Doesn't do much until json webtokens are added"""
 
-    return {'message': 'success!'}
+    return {"message": "success!"}
 
-@auth_bp.route('/createuser', methods=['POST'])
+
+# Create user route
+@auth_bp.route("/createuser", methods=["POST"])
 @cross_origin()
 def create_user():
-    ''' Creates a user when a username, password, and email.'''
+    """Creates a user when a username, password, and email."""
 
+    # Grab json content
     content = request.json
-    email = content['email']
-    passwd = content['password']
-    username = content['password']
+    email = content["email"]
+    passwd = content["password"]
+    username = content["password"]
 
-    new_user = User(
-        username=username,
-        password=passwd,
-        email=email
-    )
+    # Create user
+    new_user = User(username=username, password=passwd, email=email)
     db.session.add(new_user)
     db.session.commit()
 
-    # queried_user = db.session.query(User).filter_by(user_id=new_user.user_id).all()
-    # queried_user = process_records(queried_user)
-    return email
+    return {
+        "user_id": new_user.user_id,
+        "username": new_user.username,
+        "email": new_user.email
+    }
