600d379376b0c15e7853d50f5c2d80afbb38babe
Allie Robinson <allierob93@gmail.com>
2022-08-31 00:46:25 -0500

    delete user

---
+++ sniffr/user_routes/user_routes.py
@@ -0,0 +1,21 @@
+
+from flask import Blueprint
+from flask_cors import cross_origin
+from sniffr.models import User, db
+
+user_bp = Blueprint("user_bp", __name__)
+
+@user_bp.route("/user/<user_id>", methods=["DELETE"])
+@cross_origin()
+def delete_user(user_id):
+    queried_user = db.session.query(User).filter(User.user_id==user_id).first()
+
+    if queried_user:
+        db.session.delete(queried_user)
+        db.session.commit()
+
+        return {'message': f"Successfully deleted user"}, 410
+
+    else:
+        response = {"message": "User Not Found"}
+        return response, 404
