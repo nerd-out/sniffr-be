8c41213d0b94ed310484ef4453a0710cebf023a1
Jonathan Finger <jaefinger@gmail.com>
2022-09-11 14:47:51 -0500

    adds basic pytest conftest

---
+++ sniffr/tests/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+
+from sniffr.app import create_app
+
+# With scope=session, this function will run at the start of each session only
+@pytest.yield_fixture(scope="session")
+def app():
+    """
+    Setup our flask test app, this only gets executed once.
+
+    :return: Flask app
+    """
+    params = {
+        "DEBUG": False,
+        "TESTING": True,
+    }
+
+    _app = create_app(settings_override=params)
+
+    # Establish an application context before running the tests.
+    ctx = _app.app_context()
+    ctx.push()
+
+    yield _app
+
+    ctx.pop()
+
+
+# With scope=function, app will load for each test & info will not be shared
+@pytest.yield_fixture(scope="function")
+def client(app):
+    """
+    Setup an app client, this gets executed for each test function.
+
+    :param app: Pytest fixture
+    :return: Flask app client
+    """
+    yield app.test_client()
