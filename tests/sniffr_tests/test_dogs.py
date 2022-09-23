bbfa8834b12f95ab261ba4cb3c28722d3c6eb264
Jonathan Finger <jaefinger@gmail.com>
2022-09-22 19:00:48 -0500

    adds get dog by dog_id

--- tests/sniffr_tests/test_dogs.py
+++ tests/sniffr_tests/test_dogs.py
@@ -15,6 +15,21 @@ def test_get_all_dogs(test_client):
     assert response.status_code == 200
     assert len(content) == 3 # number of dogs in database at time of writing
 
+def test_get_one_dog(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/dog/dogid' page is sent a GET
+    THEN check that a '200' status code is returned
+    THEN check that the email returned is the same
+    """
+    # Register user using post request
+    response = test_client.get("/dog/1")
+    content = response.json
+
+    # Assert
+    assert response.status_code == 200
+    assert content['dog_name'] == 'Augie'
+
 
 def test_new_dog_register(test_existing_user_fixture, test_client):
     """
@@ -58,3 +73,50 @@ def test_new_dog_register(test_existing_user_fixture, test_client):
     assert content["dog_bio"] == create_json["dog_bio"]
     assert content["dog_pic"] == create_json["dog_pic"]
 
+# edit dog
+def test_edit_new_dog(test_existing_user_fixture, test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/dog' page is sent a POST
+    THEN check that a '200' status code is returned
+    THEN check that the info returned is the same
+    """
+    # Format json post
+    login_token = test_existing_user_fixture['token']
+    # Assemble Headers
+    headers = {"x-access-token": login_token}
+
+    # Register dog using post request
+    create_json = {
+        "dog_name": "Ein",
+        "breed_id": 110,
+        "size_id": 1,
+        "temperament_id": 2,
+        "age": '24',
+        "sex": "Data Dog",
+        "is_vaccinated": False,
+        "is_fixed": False,
+        "dog_bio": """Ein is a Pembroke Welsh Corgi and "data dog," meaning his intelligence was greatly enhanced by a research facility.""",
+        "dog_pic": "Ein driving a car pic"}
+
+    response = test_client.post("/dog", json=create_json, headers=headers)
+    content = response.json
+
+    # Check status code & that contents updated
+    assert response.status_code == 201
+    assert content["dog_name"] == create_json["dog_name"]
+    assert content["breed_id"] == create_json["breed_id"]
+    assert content["size_id"] == create_json["size_id"]
+    assert content["temperament_id"] == create_json["temperament_id"]
+    assert content["owner_id"] == 1
+    assert content["age"] == create_json["age"]
+    assert content["sex"] == create_json["sex"]
+    assert content["is_vaccinated"] == create_json["is_vaccinated"]
+    assert content["is_fixed"] == create_json["is_fixed"]
+    assert content["dog_bio"] == create_json["dog_bio"]
+    assert content["dog_pic"] == create_json["dog_pic"]
+
+# get specific dog details
+
+
+# delete dog
--- tests/sniffr_tests/test_home.py
+++
@@ -1,9 +0,0 @@
-def test_homepage(test_client):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/' page is requested (GET)
-    THEN check that the response is valid
-    """
-    response = test_client.get('/')
-    assert response.status_code == 200
-    assert b"Sniffr's" in response.data
